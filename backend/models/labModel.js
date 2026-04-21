5a9ed78ab7774f271a2827fb18486a935afc6884
Areeb Abid <areebabid12345@gmail.com>
2026-04-21 10:40:12 +0500

    Added lab model

---
+++ backend/models/labModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const labSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+module.exports = mongoose.model('Lab', labSchema);
